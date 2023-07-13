4b3d0559074dc0e53ad1406126ebbecf2b426f43
juan <juansoussev@gmail.com>
2023-07-13 14:09:45 -0300

    First Commit

---
+++ App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+
+function Section() {
+  return (
+    <>
+      <Text>asd</Text>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Text>asd</Text>
+      <Section />
+    </>
+  );
+}
+
+export default App;
