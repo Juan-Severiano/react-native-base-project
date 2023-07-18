0ae10f56cad62265fafd4bcc899c063344b1d7ee
Juan Severiano <111772146+Juan-Severiano@users.noreply.github.com>
2023-07-18 20:19:26 +0000

    asd

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+function App() {
+  return (
+    <>
+      <Text>Sejam bem vindos a Sua aplicação com React Native</Text>
+    </>
+  );
+}
+
+export default App;
